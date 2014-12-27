8c92ee293224e1616cf6e5f4e3b46501ed5d4290
Neilda <neilda@neildapacquing.com>
2014-12-27 22:23:21 +0000

    Allow editing of places only by the user who created them.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
 	def index
 		@places = Place.order("name").page(params[:page]).per(3)
@@ -20,13 +20,34 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
 	end
 
 	def update
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
 		@place.update_attributes(place_params)
-
 		redirect_to root_path
+	end
+
+	def destroy
+		# When we destroy a place we want to do three things:
+
+		# Find the right place.
+#		@place = Place.find(params[:id])
+		# Destroy the place from our database.
+#		@place.destroy
+		# Send the user to the root path.
+#		redirect_to root_path
+  @place = Place.find(params[:id])
+  @place.destroy
+
+redirect_to root_path
 	end
 
 	private
