9e6702aa68a69f9021eaceaac91ec7e7405304dc
Neilda <neilda@neildapacquing.com>
2015-01-01 19:23:47 +0000

    Error- undefined local variable or method place and comments.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,9 +13,9 @@ class PlacesController < ApplicationController
 		@place = current_user.places.create(place_params)
 		if @place.valid?
 		redirect_to root_path
-	else
+		else
 		render :new, :status => :unprocessable_entity
-	end
+		end
 	end
 
 	def show
